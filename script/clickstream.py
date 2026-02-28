04a387cbb657e7b1b187555db1ad16d86d83ad2c
Joshua Ladios <ladiosjoshua39@gmail.com>
2026-03-01 01:31:18 +0800

    Added a Schema and basic cleaning

--- script/clickstream.py
+++ script/clickstream.py
@@ -1,10 +1,10 @@
 from logger_config import logger
 from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, StringType, FloatType, TimestampType, StructField, StructType
+from pyspark.sql.functions import col, to_date, date_format
 
 spark = SparkSession.builder \
-    .appName("clickstream-pipeline") \
-    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4") \
-    .getOrCreate()
+    .appName("clickstream-pipeline").getOrCreate()
 
 spark.sparkContext.setLogLevel("ERROR")
 
@@ -16,6 +16,38 @@ hadoop_conf.set("fs.s3a.secret.key", "test")
 hadoop_conf.set("fs.s3a.path.style.access", "true")
 hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-# Use s3a:// scheme
-df = spark.read.json("s3a://clickstream-datalake/event_02-19-26.json")
-df.show()
+schema = StructType([
+    StructField("event_id", StringType(), False),
+    StructField("user_id", StringType(), False),
+    StructField("session_id", StringType(), False),
+    StructField("event_type", StringType(), False),
+    StructField("page_url", StringType(), False),
+    StructField("product_id", StringType(), False),
+    StructField("price", FloatType(), False),
+    StructField("quantity", FloatType(), True),
+    StructField("event_timestamp", StringType(), False)
+])
+
+df = spark.read.schema(schema).json("s3a://clickstream-datalake/events_02-19-26.json")
+df = (
+    df.filter(
+        col("event_id").isNotNull() &
+        col("user_id").isNotNull() &
+        col("session_id").isNotNull() &
+        col("event_type").isNotNull() &
+        col("page_url").isNotNull() &
+        col("product_id").isNotNull() &
+        col("price").isNotNull() &
+        col("event_timestamp").isNotNull()
+        )  
+        .withColumn("quantity", col("quantity").cast("int"))
+        .na.fill({"quantity": 0})
+        .withColumn("event_date", to_date("event_timestamp"))
+        .withColumn("event_time", date_format("event_timestamp", "HH:mm:ss"))
+)
+df.show(5)
+
+# from s3_utils import get_s3_client
+# if __name__ == "__main__":
+#     s3 = get_s3_client()
+#     clickstream(s3)
