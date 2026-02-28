a2c61d25881bdda268490f47aabd131ec1172073
Joshua Ladios <ladiosjoshua39@gmail.com>
2026-03-01 01:33:17 +0800

    Fixed the json format

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -3,14 +3,15 @@ import random
 from datetime import datetime, timedelta
 import pandas as pd
 import os
+import json
 
-n_users = 20
+n_users = 100
 sessions_per_user = 3
 
 products = [
-    {"product_id": "sku_1", "price": 29.99},
-    {"product_id": "sku_2", "price": 59.99},
-    {"product_id": "sku_3", "price": 99.99},
+    {"product_id": "sku_1", "price": 29.99, "category": "apparel"},
+    {"product_id": "sku_2", "price": 59.99, "category": "home"},
+    {"product_id": "sku_3", "price": 99.99, "category": "electronic"},
 ]
 
 data = []
@@ -28,11 +29,11 @@ for user in range(1, n_users + 1):
             "event_id": str(uuid.uuid4()),
             "user_id": f"user_{user}",
             "session_id": session_id,
-            "event_type": "page_view",
+            "event_type": "product_view",
             "page_url": f"/product/{product['product_id']}",
             "product_id": product["product_id"],
             "price": product["price"],
-            "timestamp": session_time.isoformat()
+            "event_timestamp": session_time.isoformat()
         })
 
         # 2️⃣ maybe add to cart
@@ -46,7 +47,8 @@ for user in range(1, n_users + 1):
                 "page_url": "/cart",
                 "product_id": product["product_id"],
                 "price": product["price"],
-                "timestamp": session_time.isoformat()
+                "quantity": random.randint(1, 3),
+                "event_timestamp": session_time.isoformat()
             })
 
             # 3️⃣ maybe purchase
@@ -60,12 +62,20 @@ for user in range(1, n_users + 1):
                     "page_url": "/checkout",
                     "product_id": product["product_id"],
                     "price": product["price"],
-                    "timestamp": session_time.isoformat()
+                    "quantity": random.randint(1, 3),
+                    "event_timestamp": session_time.isoformat()
                 })
 
 df = pd.DataFrame(data)
-file_name = f"events_{datetime.now().strftime("%m-%d-%y")}.json"
-script_dir = os.path.dirname(os.path.abspath(__file__))
-folder_path = os.path.join(script_dir, "data")
-os.makedirs(folder_path, exist_ok=True)
-df.to_json(f"{folder_path}/event_{datetime.now().strftime("%m-%d-%y")}.json", orient="records", indent=8)
+for n in range(1, 29):
+    file_name = f"events_02-{n}-26.json"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    folder_path = os.path.join(script_dir, "data")
+    os.makedirs(folder_path, exist_ok=True)
+
+    records = df.to_dict(orient="records")
+
+    with open(f"{folder_path}/{file_name}", "w") as f:
+        for record in records:
+            f.write(json.dumps(record) + "\n")
+    # df.to_json(f"{folder_path}/{file_name}", orient="records", indent=9)
