83ddffb15bed8e3cbb84260be638566fcecc045e
Joshua Ladios <ladiosjoshua39@gmail.com>
2026-03-06 01:21:19 +0800

    Added Clickstream.py

--- script/main.py
+++ script/main.py
@@ -1,5 +1,6 @@
 from s3_utils import get_s3_client, ensure_bucket
 from file_syncing import sync_files
+from clickstream import clean_and_validate
 
 BUCKET_NAME = "clickstream-datalake"
 DATA_PATH = "/clickstream/data"
@@ -8,6 +9,7 @@ def main():
     s3 = get_s3_client()
     ensure_bucket(s3, BUCKET_NAME)
     sync_files(s3, BUCKET_NAME, DATA_PATH)
+    clean_and_validate()
 
 if __name__ == "__main__":
     main()
